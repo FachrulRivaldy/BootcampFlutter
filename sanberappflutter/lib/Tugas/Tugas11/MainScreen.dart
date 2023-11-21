c4a786c174b2be7e81eba9a45dcc3322d1c879fb
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2023-11-21 08:14:53 +0700

    Update Pengumpulan Tugas 11

--- sanberappflutter/lib/Tugas/Tugas11/MainScreen.dart
+++ sanberappflutter/lib/Tugas/Tugas11/MainScreen.dart
@@ -30,104 +30,113 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: IconButton(
-              icon: const Icon(Icons.notifications_none),
-              onPressed: () {
-                showModal(context);
-              },
-            ),
-          )
-        ],
-        backgroundColor: const Color(0xFF5E73E1),
-        iconTheme: const IconThemeData(color: Colors.white),
-        title: const Text(
-          'SanberApp',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Color(0xFF5E73E1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-            backgroundColor: Color(0xFF5E73E1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-            backgroundColor: Color(0xFF5E73E1),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xFF5E73E1),
-        onTap: _onItemTapped,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF5E73E1),
+        appBar: AppBar(
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_none),
+                onPressed: () {
+                  showModal(context);
+                },
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage("assets/img/fachrul.jpg"),
-                  ),
-                  Spacer(),
-                  Text(
-                    "Fachrul Rivaldy",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    "fachrul.rivaldy@gmail.com",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              title: const Text("Home"),
-              leading: const Icon(Icons.home),
-              onTap: () {
-                showModal(context);
-              },
-            ),
-            ListTile(
-              title: const Text("Profile"),
-              leading: const Icon(Icons.person),
-              onTap: () {
-                showModal(context);
-              },
-            ),
-            ListTile(
-                title: const Text("Logout"),
-                leading: const Icon(Icons.logout),
-                onTap: () {
-                  Navigator.pushNamed(context, '/');
-                }),
+            )
           ],
+          backgroundColor: const Color(0xFF5E73E1),
+          iconTheme: const IconThemeData(color: Colors.white),
+          title: const Text(
+            'SanberApp',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Color(0xFF5E73E1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Color(0xFF5E73E1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+              backgroundColor: Color(0xFF5E73E1),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color(0xFF5E73E1),
+          onTap: _onItemTapped,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              const UserAccountsDrawerHeader(
+                accountEmail: Text("fachrul.rivaldy@gmail.com"),
+                accountName: Text("Fachrul Rivaldy"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("assets/img/fachrul.jpg"),
+                ),
+              ),
+              DrawerListTile(
+                iconData: Icons.assignment,
+                title: "Applied Jobs",
+                onTilePressed: () {
+                  showModal(context);
+                },
+              ),
+              DrawerListTile(
+                iconData: Icons.bookmark_border,
+                title: "Saved Jobs",
+                onTilePressed: () {
+                  showModal(context);
+                },
+              ),
+              const Divider(),
+              DrawerListTile(
+                iconData: Icons.settings,
+                title: "Settings",
+                onTilePressed: () {
+                  showModal(context);
+                },
+              ),
+              DrawerListTile(
+                iconData: Icons.logout,
+                title: "Logout",
+                onTilePressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  final IconData? iconData;
+  final String? title;
+  final VoidCallback? onTilePressed;
+
+  const DrawerListTile(
+      {Key? key, this.iconData, this.title, this.onTilePressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTilePressed,
+      dense: true,
+      leading: Icon(iconData),
+      title: Text(
+        title!,
+        style: const TextStyle(fontSize: 16),
       ),
     );
   }
